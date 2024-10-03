bb3dc160eda873521c5d10a6a4d1300b3f608888
Sawux2 <aandreviniciuss@gmail.com>
2024-10-03 19:34:24 -0300

    Configuração inicial completa

---
+++ next.config.js
@@ -0,0 +1,10 @@
+// next.config.js
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  experimental: {
+    appDir: true, // Habilita o uso da estrutura da pasta app/
+  },
+};
+
+module.exports = nextConfig;
