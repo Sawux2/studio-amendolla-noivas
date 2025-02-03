e0fcd618851256d1a169f7d70880cab14ee5d491
Sawux2 <aandreviniciuss@gmail.com>
2025-02-03 18:50:05 -0300

    testando indexnow

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,23 +1,24 @@
-/** @type {import('next-sitemap').IConfig} */
 module.exports = {
-  siteUrl: 'https://studioamendollanoivas.com.br', // Substitua pelo seu domínio
-  generateRobotsTxt: true, // Gera o arquivo robots.txt
-  exclude: ['/paginaSeo/*'], // Exclui todas as rotas dentro de /paginaSeo
+  siteUrl: 'https://studioamendollanoivas.com.br',
+  generateRobotsTxt: true,
+  // Não exclua as páginas da pasta /paginaSeo
+  // exclude: ['/paginaSeo/*'], // Remova ou ajuste essa linha conforme necessário
+
   transform: async (config, path) => {
-    // Remove o segmento "/paginaSeo" das URLs
+    // Ajuste para remover "/paginaSeo" apenas se necessário
     if (path.startsWith('/paginaSeo')) {
       path = path.replace('/paginaSeo', '');
     }
 
     return {
-      loc: path, // URL da página
-      lastmod: new Date().toISOString(), // Data da última modificação
-      changefreq: 'monthly', // Frequência de mudança
-      priority: 0.8, // Prioridade
+      loc: path,
+      lastmod: new Date().toISOString(),
+      changefreq: 'monthly',
+      priority: 0.8,
     };
   },
+
   additionalPaths: async (config) => {
-    // Adiciona manualmente as páginas dinâmicas
     const dynamicPages = [
       '/beleza-para-formatura-sao-paulo',
       '/casamento',
